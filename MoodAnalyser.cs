2a1e274de0ab6e6e5e0fbe72e5af93d683097e22
syedfaizz <syedfaizz45@gmail.com>
2021-08-14 09:45:11 +0530

    [faisal] Add: uc1-mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public static string AnalyseMood(string message)
+        {
+            message = message.ToLower();
+            if (message.Contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
